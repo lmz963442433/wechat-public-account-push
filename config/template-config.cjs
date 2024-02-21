ee00259fce0dceb3d8e240f3b7df2302fd8d1d4f
lmz963442433 <111717500+lmz963442433@users.noreply.github.com>
2024-02-21 15:41:00 +0800

    Update template-config.cjs

--- config/template-config.cjs
+++ config/template-config.cjs
@@ -1,7 +1,7 @@
 /* eslint-disable */
 const TEMPLATE_CONFIG = [
   {
-    id: '2Z7s4jkkuuZD2QLqPfsOkuCOHj9bd11JVc94E48Kgc0',
+    id: '0001',
     title: '亲爱的, 早上好',
     desc: `
       **{{date.DATA}}**

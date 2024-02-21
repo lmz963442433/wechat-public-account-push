6163154d49bf90b16c8bff93157ce7f7e6edf32d
lmz963442433 <111717500+lmz963442433@users.noreply.github.com>
2024-02-21 15:04:57 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -16,18 +16,18 @@ const USER_CONFIG = {
   APP_SECRET: '78d57482d1becc185c0a28f08966480e',
 
   PROVINCE: '湖北',
-  CITY: '武汉',
+  CITY: '襄阳',
 
   USERS: [
     {
       // 想要发送的人的名字
       name: '石屁王',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '2ff51b57092c488682d00b3424656f4c',
+      id: 'ov_HO6QOoOFicul6COYPz-lTGuaw',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '0001',
+      useTemplateId: '2Z7s4jkkuuZD2QLqPfsOkuCOHj9bd11JVc94E48Kgc0',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '10-31',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
@@ -59,7 +59,7 @@ const USER_CONFIG = {
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '973105775afd46c095e3ffb87895297c',
+      id: 'ov_HO6XmZF0LKKegLHOrfZOJkO4A',
     }
   ],
 
